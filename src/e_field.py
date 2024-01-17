8774d08daa2e0ceb0f33c4672498c7694415e675
John Christensen <johnedchristensen@gmail.com>
2024-01-17 14:17:25 -0600

    non-physically accurate movement

--- src/e_field.py
+++ src/e_field.py
@@ -20,9 +20,15 @@ def update(self: CanvasManager, t):
     p1 = particle(WIDTH // 2, HEIGHT // 2 + particle_distance // 2, 1)
     p2 = particle(WIDTH // 2, HEIGHT // 2 - particle_distance // 2, -1)
 
-    for x in range(WIDTH):
-        for y in range(HEIGHT):
+    for x in range(HEIGHT):
+        for y in range(WIDTH):
 
+            #update
+            scale = .005
+            p1.x = p1.x +  scale * m.cos(t/1000)
+            p1.y = p1.y +  scale * m.sin(t/1000)
+            p2.y = p2.y +  scale * m.cos(t/1000)
+            p2.y = p2.y +  scale * m.sin(t/1000)
 
             #calculate
             pixel_p1_distance = p1.distance(x, y)
@@ -39,7 +45,6 @@ def update(self: CanvasManager, t):
             p2_field_strength = p2.charge / pixel_p2_distance**2
 
             total_field = p1_field_strength + p2_field_strength
-            #update
             #draw
             r,g,b,a = value_to_color(total_field, p2.charge, p1.charge)
             img.pixels[x][y] =(r,g,b,255)
@@ -53,8 +58,8 @@ canvasManager = CanvasManager(update)
 
 @dataclass
 class particle:
-    x: int
-    y: int
+    x: float
+    y: float
     charge: float
 
     def distance(self, xp, yp):
@@ -74,14 +79,21 @@ def map_value(value, range1_min, range1_max, range2_min, range2_max):
 
 
 def value_to_color(value, min_value, max_value,exponential=.99)->Tuple[int,int,int,int]:
-    val_range = max_value-min_value
-    if abs(value) < val_range * .001:
-    # if value==0:
-        return (0, 0, 0, 255)  # Black
+    # value_range = max_value-min_value
+    # if abs(value) < min_value + (value_range * .001):
+    # # if value==0:
+    #     return (0, 0, 0, 255)  # Black
 
     # Assuming full intensity colors: blue for negative, red for positive
     blue = (0, 0, 255, 255)
     red = (255, 0, 0, 255)
+    if abs(value) > max_value:
+        if value>0:
+            return red
+        else:
+            return blue
+    if abs(value) < min_value:
+        return (0,0,0,255)
 
     # Normalize value to [0, 1]
     if value < 0:
